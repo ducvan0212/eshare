f825ea953fccd3ae59ae76175409be451b2f2a34
ducvan0212 <ducvan0212@gmail.com>
2013-01-30 20:54:36 +0700

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eshare::Application.config.secret_token = 'b9cd48d41461310d47879ff770438803f6324b61cfb20f10c4f7cc93aa192c39b59d9fd794b7b3c9a24a4d05f0ab68ce4fd9246c99d21fb912f7d14dbf53c030'
