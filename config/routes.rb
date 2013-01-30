28aa05cd17e8ae47efaa0015a635e604e11db896
ducvan0212 <ducvan0212@gmail.com>
2013-01-30 21:42:59 +0700

    help page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Eshare::Application.routes.draw do
   match '/about', to: 'static_pages#about'
-     
+  match '/help', to: 'static_pages#help'   
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
