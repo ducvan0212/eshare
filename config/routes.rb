8b3a8f68c11ac1a934ab81b57b7cae2cf4bb08ac
ducvan0212 <ducvan0212@gmail.com>
2013-02-02 22:36:43 +0700

    sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Eshare::Application.routes.draw do
-  get "users/new"
-
   root to: 'static_pages#home'
   
   match '/about', to: 'static_pages#about'
-  match '/help', to: 'static_pages#help'   
+  match '/help', to: 'static_pages#help'
+
+  resources :users
+  match '/signup', to: 'users#new'   
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
