a1d7694d7b2c0d7afe00709e2ab4ebe883946d6b
ducvan0212 <ducvan0212@gmail.com>
2013-05-06 11:05:29 +0700

    delete bookmark

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -12,6 +12,8 @@ class BookmarksController < ApplicationController
   def destroy
     @exam_paper = Bookmark.find(params[:id]).exam_paper
     current_user.rm_bookmark!(@exam_paper)
+    @user = @exam_paper.user
+    @bookmarks = @user.bookmarks
     respond_to do |format|
       format.js
     end
